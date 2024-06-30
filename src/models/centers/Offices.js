f28a52c436f2cca8ace29a484c495960755a755f
Fiagapcoo <filipeaguilar08@gmail.com>
2024-06-30 19:50:36 +0100

    fix the last commit

--- src/models/centers/Offices.js
+++ src/models/centers/Offices.js
@@ -1,3 +1,4 @@
+
 module.exports = (sequelize, DataTypes) => {
     const Offices = sequelize.define('Offices', {
         office_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
@@ -8,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
         timestamps: false
     });
 
-    OfficeWorkers.associate = function(models) {
-        OfficeWorkers.belongsTo(models.OfficeAdmins, { foreignKey: 'office_id' });
-        OfficeWorkers.belongsTo(models.OfficeWorkers, { foreignKey: 'office_id' });
+    Offices.associate = function(models) {
+        Offices.belongsTo(models.OfficeAdmins, { foreignKey: 'office_id' });
+        Offices.belongsTo(models.OfficeWorkers, { foreignKey: 'office_id' });
         
     };
 
