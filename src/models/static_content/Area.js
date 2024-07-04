b197b68643820fda4dfd33d41243811ae8be7158
Fiagapcoo <filipeaguilar08@gmail.com>
2024-07-04 01:00:36 +0100

    change icon_name path to 255

--- src/models/static_content/Area.js
+++ src/models/static_content/Area.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const Area = sequelize.define('Area', {
         area_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
         title: { type: DataTypes.STRING(255), allowNull: false, unique: true },
-        icon_name: {type: DataTypes.STRING(50), allowNull:false}
+        icon_name: {type: DataTypes.STRING(255), allowNull:false}
     }, {
         schema: 'static_content',
         tableName: 'area',
