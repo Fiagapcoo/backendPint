86346937f7504825b593aaab2f9efb2a0890e389
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-07-08 22:44:57 +0100

    try both ways

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -4,9 +4,9 @@ const crypto = require("crypto");
 
 //for mobile
 const algorithm = "aes-256-cbc";
-// const key = Buffer.from(process.env.ENCRYPTION_KEY, "base64");
+const key = Buffer.from(process.env.ENCRYPTION_KEY, "base64");
 // const iv = Buffer.from(process.env.ENCRYPTION_IV, "base64");
-const key = crypto.randomBytes(32); // 256-bit key
+//const key = crypto.randomBytes(32); // 256-bit key
 const iv = crypto.randomBytes(16);  // 128-bit IV
 
 const encrypt = (text) => {
