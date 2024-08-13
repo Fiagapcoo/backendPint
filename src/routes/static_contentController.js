abe02b4199ee80c6a9211d7a70144ba09c47bcf6
Fiagapcoo <filipeaguilar08@gmail.com>
2024-08-14 00:56:47 +0100

    add edit and fix the select of the subArea

--- src/routes/static_contentController.js
+++ src/routes/static_contentController.js
@@ -8,6 +8,7 @@ router.post('/create-sub-category', validation, static_contentController.create_
 router.get('/get-areas', validation, static_contentController.get_all_areas);
 router.get('/get-sub-areas', validation, static_contentController.get_all_sub_areas);
 router.patch('/update-category/:categoryID', validation, static_contentController.update_category);
-// router.patch('/update-sub-category/:subCategoryID', validation, static_contentController.update_sub_category);
+router.patch('/update-sub-category/:subCategoryID', validation, static_contentController.update_sub_category);
 router.delete('/delete-category/:categoryID', validation, static_contentController.delete_category);
+router.delete('/delete-sub-category/:subCategoryID', validation, static_contentController.delete_sub_category);
 module.exports = router;
