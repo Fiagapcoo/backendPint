5f08a7b00194f1a10d1a1dfec47b71c510cbe776
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-08-14 20:18:05 +0100

    to ease up testing

---
+++ src/start/TESTING.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+const insertContentForTests = async () => {
+  try {
+    await db.sequelize.authenticate();
+    await db.sequelize.query(`
+      INSERT INTO "dynamic_content"."posts" ("sub_area_id", "office_id", "publisher_id", "title", "content", "type", "creation_date")
+      VALUES
+      (4002, 4, 18, 'le post for le test', 'cacacacacaca', 'P',CURRENT_TIMESTAMP),
+      (4002, 4, 18, 'le post for le test2', 'cacacacacaca', 'P',CURRENT_TIMESTAMP),
+      (4002, 4, 18, 'le post for le test3', 'cacacacacaca', 'P',CURRENT_TIMESTAMP);
+  `);
+    await db.sequelize.query(`
+      INSERT INTO "dynamic_content"."events" ("publisher_id", "office_id", "sub_area_id", "name", "description", "event_location","event_date", "recurring","creation_date")
+      VALUES
+      (18, 2, 1001, 'VOLUNTARIADO SERA?', 'VOLUNTARIATE PARA SERES BOA PESSOA', '40.65056885039045 -7.905631258488603','2023-11-15', false,CURRENT_TIMESTAMP),
+      (18, 2, 1001, 'VOLUNTARIADO SERA?', 'DOA SANGUE NAO SEJAS ESTRANHO', '40.65056885039045 -7.905631258488603','2023-11-15', false,CURRENT_TIMESTAMP);
+      
+  `);
+    await db.sequelize.query(`
+      INSERT INTO "dynamic_content"."forums" ("publisher_id", "office_id", "sub_area_id", "title", "content","creation_date")
+      VALUES
+      (18, 1, 4002, 'FORUM TEST', 'VIBES? WHAT ARE THOSE',CURRENT_TIMESTAMP),
+      (18, 5, 1002, 'PRECISO DFE AJUDO A TESTAR', 'TESTES E MAIKS TESTES',CURRENT_TIMESTAMP);
+  `);
+    console.log("All models were cleaned successfully.");
+  } catch (error) {
+    console.error("Unable to sync models:", error);
+  } 
+};
+
+insertContentForTests();
