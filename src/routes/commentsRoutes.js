22f36d56517e30d3b1a1240acd144e8887c61a64
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-08-21 14:31:37 +0100

    added route to delete like from content

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -7,7 +7,7 @@ router.post('/add-comment', validation, controller.add_comment);
 router.get('/get-comment-tree/content/:contentType/id/:contentID', validation, controller.get_comments_tree);
 
 router.post('/add-like', validation, controller.like_comment);
-//router.delete('/remove-like', validation, controller.unlike_comment);
+router.delete('/remove-like', validation, controller.unlike_comment);
 router.post('/report-coment', validation, controller.report_comment);
 
 module.exports = router;
