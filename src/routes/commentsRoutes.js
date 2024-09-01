44d7fe56bd7a0d527e9afb1fffd0fdbf93abeb09
Fiagapcoo <filipeaguilar08@gmail.com>
2024-09-01 21:14:13 +0100

    AXIOS IS BUMB

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -8,6 +8,7 @@ router.get('/get-comment-tree/content/:contentType/id/:contentID', validation, c
 router.delete('/delete-comment/:commentID', validation, controller.delete_comment);
 router.post('/add-like', validation, controller.like_comment);
 router.delete('/remove-like', validation, controller.unlike_comment);
+router.patch('/remove-like', validation, controller.unlike_comment);
 router.post('/report-coment', validation, controller.report_comment);
 router.get('/get-likes-per-user/:userID', validation, controller.likes_per_user);
 router.get('/get-likes-per-content/content/:contentType/id/:contentID/', validation, controller.likes_per_content);
