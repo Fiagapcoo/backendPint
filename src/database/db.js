87b588a81815e4a90f9c295807a870f894779049
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-09-02 22:07:01 +0100

    trying to fix an error

--- src/database/db.js
+++ src/database/db.js
@@ -9,6 +9,12 @@ const sequelize = new Sequelize({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     logging: console.log, // Enable logging to see SQL queries
+    pool: {
+        max: 15,       // Increase max connections
+        min: 5,        // Ensure a minimum number of connections are always available
+        acquire: 60000, // Increase acquire timeout
+        idle: 10000    // Time before an idle connection is released
+      }
 });
 
 module.exports = sequelize;
