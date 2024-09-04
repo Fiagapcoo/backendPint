abd0e3f731f0cbbdf21df65a9395ec51e7267d24
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-09-04 22:25:58 +0100

    fix

--- src/routes/warningsRoutes.js
+++ src/routes/warningsRoutes.js
@@ -6,3 +6,6 @@ const {validation} = require('../controllers/jwt_middlewareController');
 const warningController = require('../controllers/warningController');
 
 router.get('/:office_ID', validation, warningController.warnings_per_office);
+
+
+module.exports = router;
